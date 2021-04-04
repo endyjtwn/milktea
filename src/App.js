87f265fbcea836d7b1750404d0862d600139d7ac
endyjtwn <endyjtwn@gmail.com>
2021-04-04 17:39:46 +0200

    fix bug tone and added ios

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import About from "./About";
 
 import noteValues from "./noteValues.json";
 
+import * as Tone from "tone";
+
+
 const noteMapArray = Object.keys(noteValues).map((note) => ({
   note,
   freq: noteValues[note],
@@ -24,7 +27,7 @@ function App() {
   const [melody, setMelody] = useState([]);
   const [status, setStatus] = useState("stop");
 
-  let acl = new Accelerometer({ frequency: 5 });
+  // let acl = new Accelerometer({ frequency: 5 });
   const [result, setResult] = useState("");
 
   useEffect(() => {
@@ -44,48 +47,87 @@ function App() {
   }, [note, status]);
 
   function getAccelerometer() {
-    acl.addEventListener("reading", () => {
-      if (acl.x <= -2) {
-        console.log(acl.x, acl.y, acl.z);
 
-        if (acl.x > -3 && acl.x <= -2) {
+    window.ondevicemotion = function (event) {
+      var accelerationX = event.accelerationIncludingGravity.x;
+      var accelerationY = event.accelerationIncludingGravity.y;
+      var accelerationZ = event.accelerationIncludingGravity.z;
+
+      if (accelerationX <= -2) {
+
+        if (accelerationX > -3 && accelerationX <= -2) {
           const pos = Math.floor(Math.random() * (51 - 35 + 1)) + 35;
-          console.log(22222, acl.x + acl.y + acl.z + pos);
+          console.log(22222, accelerationX, pos, note);
           if (note) setNote(noteMapArray[pos].note);
 
 
-        } else if (acl.x > -4 && acl.x <= -3) {
+        } else if (accelerationX > -4 && accelerationX <= -3) {
           const pos = Math.floor(Math.random() * (68 - 52 + 1)) + 52;
-          console.log(333333, acl.x, pos);
+          console.log(333333, accelerationX, pos, note);
           if (note) setNote(noteMapArray[pos].note);
 
 
-        } else if (acl.x > -5 && acl.x <= -4) {
+        } else if (accelerationX > -5 && accelerationX <= -4) {
           const pos = Math.floor(Math.random() * (85 - 69 + 1)) + 69;
-          console.log(444444, acl.x, pos);
+          console.log(444444, accelerationX, pos);
           if (note) setNote(noteMapArray[pos].note);
 
 
         } else {
           const pos = Math.floor(Math.random() * (34 - 18 + 1)) + 18;
           if (note) setNote(noteMapArray[pos].note);
-          console.log('000000', acl.x, pos);
+          console.log('000000', accelerationX, pos);
         }
       }
-    });
-    acl.start();
+    }
+
+    // acl.addEventListener("reading", () => {
+
+
+    // console.log('000000', acl.x, pos);
+    // if (acl.x <= -2) {
+    //   console.log(acl.x, acl.y, acl.z);
+
+    //   if (acl.x > -3 && acl.x <= -2) {
+    //     const pos = Math.floor(Math.random() * (51 - 35 + 1)) + 35;
+    //     console.log(22222, acl.x + acl.y + acl.z + pos);
+    //     if (note) setNote(noteMapArray[pos].note);
+
+
+    //   } else if (acl.x > -4 && acl.x <= -3) {
+    //     const pos = Math.floor(Math.random() * (68 - 52 + 1)) + 52;
+    //     console.log(333333, acl.x, pos);
+    //     if (note) setNote(noteMapArray[pos].note);
+
+
+    //   } else if (acl.x > -5 && acl.x <= -4) {
+    //     const pos = Math.floor(Math.random() * (85 - 69 + 1)) + 69;
+    //     console.log(444444, acl.x, pos);
+    //     if (note) setNote(noteMapArray[pos].note);
+
+
+    //   } else {
+    //     const pos = Math.floor(Math.random() * (34 - 18 + 1)) + 18;
+    //     if (note) setNote(noteMapArray[pos].note);
+    //     console.log('000000', acl.x, pos);
+    //   }
+    // }
+    // });
+    // acl.start();
+    Tone.start()
     setStatus("start");
   }
 
   function waitAccelerometer() {
     setStatus("wait");
+    Tone.Transport.stop();
     setMelody([]);
   }
 
   function stopAccelerometer() {
-    acl.stop();
+    // acl.stop();
     setStatus("stop");
-    setNote(null);
+    // setNote(null);
   }
 
   return (
